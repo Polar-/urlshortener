3600a6e56b752afb8abd5d907e3aec95596418ef
Polar- <j.piirainen@hotmail.fi>
2015-03-30 13:01:11 +0300

    Update urlshort.sql

--- sql_mysql/urlshort.sql
+++ sql_mysql/urlshort.sql
@@ -7,23 +7,3 @@ CREATE TABLE url (
     longurl TEXT,
     shorturl VARCHAR(20)
 )ENGINE=InnoDB;
-
-
-CREATE TRIGGER shorturl_gen AFTER INSERT ON url
-  FOR EACH ROW
-    INSERT INTO sUrl(shorturl, longurl) VALUES (
-        CONCAT(LEFT(NEW.longurl, 4), RIGHT(NEW.longurl, 3)), NEW.id
-        );
-
-
-
-
-///
-INSERT INTO url(longurl, shorturl) VALUES (
-    "www.iltalehti.fi", "k9sd5JDFN"
-);
-
-INSERT INTO url(longurl, shorturl) VALUES (
-    "iltalehti.fi", "k9djfnSK"
-);
-///
